1ce0ff6d9468f1d3452bae42bf47c5a0766d4244
abuamar142 <amarjabon130@gmail.com>
2022-05-31 07:49:38 +0700

    penambahan deskripsi fitur

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ connection = sqlite3.connect("database.db")
 cursor = connection.cursor()
 
 def createDatabase():
+    """ sebuah fitur untuk membuat tabel database yang didalamnya ada nama dan harga barang """
     sqlite_create_table_query = '''CREATE TABLE data_barang (
                                 Jenis TEXT,
                                 Nama TEXT,
@@ -17,6 +18,7 @@ def createDatabase():
     cursor.close()
 
 def getMenu(jenis):
+    """ sebuah fitur untuk mengambil data dari database berdasarkan jenis barang """
     try:
         get = cursor.execute("SELECT * FROM data_barang WHERE Jenis=?", (jenis,))
         fetch = get.fetchall()
@@ -26,6 +28,7 @@ def getMenu(jenis):
         print(e)
 
 def updateMenu(namaAwal, namaGanti):
+    """ sebuah fitur untuk mengganti nama barang dengan parameter nama sekarang dan nama yang akan menjadi ganti """
     try:
         cursor.execute("UPDATE data_barang SET Nama=? WHERE Nama=?", (namaGanti, namaAwal))
         connection.commit()
@@ -34,6 +37,7 @@ def updateMenu(namaAwal, namaGanti):
         print(e)
 
 def getNama(index):
+    """ sebuah fitur untuk mengambil nama barang berdasarkan kode dari barang """
     try:
         get = cursor.execute("SELECT * FROM data_barang WHERE Kode=?", (index,))
         fetch = get.fetchall()
@@ -42,9 +46,10 @@ def getNama(index):
     except sqlite3.Error as e:
         print(e)
 
-def getHarga(index):
+def getHarga(namaBarang):
+    """ sebuah fitur untuk mengambil harga barang berdasarkan nama barang """
     try:
-        get = cursor.execute("SELECT * FROM data_barang WHERE Kode=?", (index,))
+        get = cursor.execute("SELECT * FROM data_barang WHERE Nama=?", (namaBarang,))
         fetch = get.fetchall()
         return fetch[0][2]
 
